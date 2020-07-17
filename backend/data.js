259ff3845c2dfdae2efde320b7eb1210aff43392
phdev182 <phdev182@gmail.com>
2020-07-17 08:24:58 -0400

    criando backend

---
+++ backend/data.js
@@ -0,0 +1,75 @@
+
+export default {
+  products:[
+    {
+    _id: '1',
+    name: 'Ac/Dc',
+    category: 'Shirts',
+    image: '/images/camisetas/acdc.jpg',
+    price: 65,
+    brand: 'Volt', 
+    rating: 4.5,
+    numReviews: 10
+  },
+  {
+    _id: '2',
+    name: 'Metalica',
+    category: 'Shirts',
+    image: '/images/camisetas/metalica.jpg',
+    price: 60,
+    brand: 'Volt', 
+    rating: 4.5,
+    numReviews: 10
+  },
+  {
+    _id: '3',
+    name: 'Slipknot',
+    category: 'Shirts',
+    image: '/images/camisetas/slipknot.jpg',
+    price: 70,
+    brand: 'Volt', 
+    rating: 4,
+    numReviews: 30
+  },
+  {
+    _id: '4',
+    name: 'Red Hot',
+    category: 'Shirts',
+    image: '/images/camisetas/redhot.jpg',
+    price: 70,
+    brand: 'Volt', 
+    rating: 4,
+    numReviews: 5
+  },
+  {
+    _id: '5',
+    name: 'Beatles',
+    category: 'quadros',
+    image: '/images/quadros/beatles.jpg',
+    price: 50,
+    brand: 'Delemans', 
+    rating: 4,
+    numReviews: 8
+  },
+  {
+    _id: '6',
+    name: 'Hendrix',
+    category: 'quadros',
+    image: '/images/quadros/hendrix.jpg',
+    price: 50,
+    brand: '4you', 
+    rating: 4,
+    numReviews: 8
+  },
+  {
+    _id: '7',
+    name: 'Pink Floyd',
+    category: 'quadros',
+    image: '/images/quadros/pinkfloyd.jpg',
+    price: 50,
+    brand: '4you', 
+    rating: 4,
+    numReviews: 8
+  },
+]
+}
